d07f6a747828db6b636fdead844884f724f82ff4
KokilaSuresh03 <kokila0392@gmail.com>
2023-04-30 20:51:02 +0530

    First push

---
+++ src/test/java/org/hcl/Class1.java
@@ -0,0 +1,13 @@
+package org.hcl;
+
+public class Class1 {
+private void test1() {
+	System.out.println("1");
+}
+private void test2() {
+	System.out.println("2");
+}
+private void test3() {
+	System.out.println("3");
+}
+}
